6090793317ec635891d2f62dedfeae500195e5ee
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2023-02-24 21:39:09 +0300

    added datepicker to AddIncomeScreen

--- FamilyFinanceService/app/src/main/java/com/example/familyfinanceservice/AddIncomeScreen.kt
+++ FamilyFinanceService/app/src/main/java/com/example/familyfinanceservice/AddIncomeScreen.kt
@@ -1,18 +1,27 @@
 package com.example.familyfinanceservice
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.util.TypedValue
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.lang.Float
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddIncomeScreen : AppCompatActivity() {
+
+    var formatDate = SimpleDateFormat("dd MMMM YYYY", Locale.US)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_income_screen)
@@ -27,6 +36,8 @@ class AddIncomeScreen : AppCompatActivity() {
         removeCategoryButton.setOnClickListener {
             showRemoveCategoryDialog()
         }
+
+
     }
 
     fun showAddCategoryDialog(){
@@ -64,5 +75,20 @@ class AddIncomeScreen : AppCompatActivity() {
         builder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
 
         builder.show()
+    }
+
+    fun openDatePicker(view: View){
+        val getDate = Calendar.getInstance()
+        val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ datePicker, i, i2, i3 ->
+            val selectDate = Calendar.getInstance()
+            selectDate.set(Calendar.YEAR, i)
+            selectDate.set(Calendar.MONTH, i2)
+            selectDate.set(Calendar.DAY_OF_MONTH, i3)
+            val date = formatDate.format(selectDate.time)
+            Toast.makeText(this, "Date: " + date, Toast.LENGTH_SHORT).show()
+            val datePickerButton = findViewById<Button>(R.id.datePickerButton)
+            datePickerButton.text = date
+        }, getDate.get(Calendar.YEAR), getDate.get(Calendar.MONTH), getDate.get(Calendar.DAY_OF_MONTH))
+        datePicker.show()
     }
 }
