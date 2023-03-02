cf6c5a90be1fd42971a884697cc6eebe06fbc847
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2023-03-02 10:32:24 +0300

    fixed AddExpensesScreen.kt

--- FamilyFinanceService/app/src/main/java/com/etu/familyFinanceService/AddExpensesScreen.kt
+++ FamilyFinanceService/app/src/main/java/com/etu/familyFinanceService/AddExpensesScreen.kt
@@ -12,7 +12,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.example.familyfinanceservice.R
+import com.etu.familyFinanceService.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.text.SimpleDateFormat
 import java.util.*
