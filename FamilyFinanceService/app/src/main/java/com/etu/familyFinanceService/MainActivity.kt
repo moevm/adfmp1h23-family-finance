f24b355a7ae2c98ea253500dc58873f75f4d46a7
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2023-03-02 10:33:56 +0300

    fixed MainActivity.kt

--- FamilyFinanceService/app/src/main/java/com/etu/familyFinanceService/MainActivity.kt
+++ FamilyFinanceService/app/src/main/java/com/etu/familyFinanceService/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import com.example.familyfinanceservice.R
+import com.etu.familyFinanceService.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
