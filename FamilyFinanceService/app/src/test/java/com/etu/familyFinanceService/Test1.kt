fd6f0c878109ed03dbecb18b24395a07de3dfd1d
deimosdomino <Deimo.s@yandex.ru>
2023-03-12 21:53:02 +0300

    add Test 1

---
+++ FamilyFinanceService/app/src/test/java/com/etu/familyFinanceService/Test1.kt
@@ -0,0 +1,25 @@
+package com.etu.familyFinanceService
+
+import com.etu.utils.formatDate
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(MockitoJUnitRunner::class)
+class Test1 {
+
+
+    @Test
+    fun test() {
+        val date = Date(0)
+        assert(formatDate(date) == "01 January 1970")
+    }
+}
