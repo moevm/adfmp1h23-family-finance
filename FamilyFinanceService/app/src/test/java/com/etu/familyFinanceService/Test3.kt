382e9f5f61323db8fc19c89ed0d1318f6ecd788a
deimosdomino <Deimo.s@yandex.ru>
2023-03-12 21:53:14 +0300

    add Test 3

---
+++ FamilyFinanceService/app/src/test/java/com/etu/familyFinanceService/Test3.kt
@@ -0,0 +1,33 @@
+package com.etu.familyFinanceService
+
+import com.etu.classes.ExpensesList
+import com.etu.classes.IncomesList
+import com.etu.utils.formatDate
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(MockitoJUnitRunner::class)
+class Test3 {
+
+
+    @Test
+    fun test() {
+        val expenses: ExpensesList = ExpensesList();
+        expenses.addIncome("test", 100)
+        expenses.addIncome("testtest", 100)
+        assert(expenses.expenses.get(0).first == "test")
+        assert(expenses.expenses.get(0).second == 100)
+        expenses.editIncome(0, "asdasd", 20)
+        assert(expenses.expenses.get(0).first == "asdasd")
+        assert(expenses.expenses.get(0).second == 20)
+    }
+}
